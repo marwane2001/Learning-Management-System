12dbef180d4ca1e735cd6af50094f9011f5031be
marwanesk <marwanebenhadj@gmail.com>
2025-04-21 00:48:16 +0100

    custom userauth

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,6 +126,7 @@ STATIC_FILES_DIR=[os.path.join(BASE_DIR,"static")]
 STATIC_ROOT= BASE_DIR/'templates'
 MEDIA_URL='/media/' #localhost/media/x.jpg
 MEDIA_ROOT=BASE_DIR/'media'
+AUTH_USER_MODEL='userauths.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    username = models.CharField(unique=True, max_length=100)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=100) 
+    otp = models.CharField(max_length=100, null=True, blank=True ,unique=True)  
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'full_name']
+
+    def __str__(self):
+        return self.email
+
+    def save(self, *args, **kwargs):
+        if self.email:  # Check if email exists
+            email_username = self.email.split("@")[0]
+            if not self.full_name:
+                self.full_name = email_username
+            if not self.username:
+                self.username = email_username
+        super(User, self).save(*args, **kwargs)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.FileField(upload_to="user_folder", default="default-user.jpg", null=True, blank=True)
+    full_name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    about = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name if self.full_name else str(self.user.full_name)from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    username = models.CharField(unique=True, max_length=100)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=100)  # Removed unique=True for practical use
+    otp = models.CharField(max_length=100, null=True, blank=True)  # Removed unique=True
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'full_name']
+
+    def __str__(self):
+        return self.email
+
+    def save(self, *args, **kwargs):
+        if self.email:  # Check if email exists
+            email_username = self.email.split("@")[0]
+            if not self.full_name:
+                self.full_name = email_username
+            if not self.username:
+                self.username = email_username
+        super(User, self).save(*args, **kwargs)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.FileField(upload_to="user_folder", default="default-user.jpg", null=True, blank=True)
+    full_name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    about = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name if self.full_name else str(self.user.full_name)
