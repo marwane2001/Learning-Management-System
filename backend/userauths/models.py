36d1cc5ccb8663c05282d513c6cd0e7d64d55aa4
marwanesk <marwanebenhadj@gmail.com>
2025-04-21 17:57:09 +0100

    tweaks on userauths#2

--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 
 class User(AbstractUser):
     username = models.CharField(unique=True, max_length=100)
@@ -37,3 +38,11 @@ class Profile(models.Model):
         if not self.full_name:
             self.full_name = self.user.username if self.user.username else "Anonymous"
         super(Profile, self).save(*args, **kwargs)
+
+def create_user_profile(sender,instance,created,**kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+def save_user_profile(sender,instance,**kwargs):
+    instance.profile.save()
+post_save.connect(create_user_profile,sender=User)
+post_save.connect(save_user_profile,sender=User)
