9ca7e945be02b888de55dd0aa2ae77b802fa5013
marwanesk <marwanebenhadj@gmail.com>
2025-04-24 17:43:31 +0100

    password reset view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("user/token/refresh/", TokenRefreshView.as_view()),
     path("user/register/", api_views.RegisterView.as_view()),
     path("user/password-reset-email/<email>/", api_views.PasswordResetEmailVerifyAPIView.as_view()),
+    path("user/password-change/", api_views.PasswordChangeAPIView.as_view()),
+
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from requests import Response
+
 from api import serializer as api_serializer
 from userauths.models import User, Profile
 import random
@@ -41,3 +43,21 @@ class PasswordResetEmailVerifyAPIView(generics.RetrieveAPIView):
             print("link =====", link)
 
             return user
+class PasswordChangeAPIView(generics.CreateAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = api_serializer.UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        otp = request.data['otp']
+        uuidb64 = request.data['uuidb64']
+        password = request.data['password']
+
+        user = User.objects.get(id=uuidb64, otp=otp)
+        if user:
+            user.set_password(password)
+            user.otp = ""
+            user.save()
+
+            return Response({"message": "Password Changed Successfully"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message": "User Does Not Exist"}, status=status.HTTP_404_NOT_FOUND)
