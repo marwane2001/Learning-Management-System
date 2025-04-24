20eddc23595b79d5e84379a68f304919bc7b6dfc
marwanesk <marwanebenhadj@gmail.com>
2025-04-24 17:37:45 +0100

    password reset

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("user/token/", api_views.MyTokenObtainPairView.as_view()),
     path("user/token/refresh/", TokenRefreshView.as_view()),
     path("user/register/", api_views.RegisterView.as_view()),
+    path("user/password-reset-email/<email>/", api_views.PasswordResetEmailVerifyAPIView.as_view()),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from api import serializer as api_serializer
 from userauths.models import User, Profile
-
+import random
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = api_serializer.MyTokenObtainPairSerializer
@@ -13,3 +14,30 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = [AllowAny]
     serializer_class = api_serializer.RegisterSerializer
+
+def generate_random_otp(Length=7):
+    otp = ''.join([str(random.randint(0, 9)) for _ in range(Length)])
+    return otp
+
+class PasswordResetEmailVerifyAPIView(generics.RetrieveAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = api_serializer.UserSerializer
+
+    def get_object(self):
+        email = self.kwargs['email']
+
+        user = User.objects.filter(email=email).first()
+
+        if user:
+            uuidb64 = user.pk
+            refresh = RefreshToken.for_user(user)
+            refresh_token = str(refresh.access_token)
+
+            user.refresh_token = refresh_token
+            user.otp = generate_random_otp()
+            user.save()
+
+            link = f"http://localhost:5173/create-new-password/?otp={user.otp}&uuidb64={uuidb64}&=refresh_token={refresh_token})"
+            print("link =====", link)
+
+            return user
