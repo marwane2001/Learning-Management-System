2edacf1ac8868e68d4433f202c0d6d330c675f29
marwanesk <marwanebenhadj@gmail.com>
2025-04-24 16:29:24 +0100

    registration finished

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from userauths.models import Profile, User
+from django.contrib.auth.password_validation import validate_password
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
@@ -12,6 +13,34 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token['username'] = user.username
 
         return token
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['full_name', 'email', 'password', 'password2']
+
+    def validate(self, attr):
+        if attr['password'] != attr['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+
+        return attr
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            full_name=validated_data['full_name'],
+            email=validated_data['email'],
+        )
+
+        email_username, _ = user.email.split("@")
+        user.username = email_username
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -7,7 +7,6 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     full_name = models.CharField(max_length=100)  # Removed unique=True
     otp = models.CharField(max_length=100, null=True, blank=True)
-    refresh_token = models.CharField(max_length=1000, null=True, blank=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
