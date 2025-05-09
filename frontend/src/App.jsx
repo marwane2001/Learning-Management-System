c6cf95ff20d9d8e4f661dedcae70d6ce3d8ebf99
marwanesk <marwanebenhadj@gmail.com>
2025-05-09 12:56:51 +0100

    added teamplate for the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import MainWrapper from "./layouts/MainWrapper";
+import Register from "./views/auth/Register";
+import { CartContext } from "./views/plugin/Context";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cartCount, setCartCount] = useState(0); // Default state for cart count
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <CartContext.Provider value={[cartCount, setCartCount]}>
+      <BrowserRouter>
+        <MainWrapper>
+          <Routes>
+            <Route path="/register/" element={<Register />} />
+          </Routes>
+        </MainWrapper>
+      </BrowserRouter>
+    </CartContext.Provider>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/store/auth.js
+++ frontend/src/store/auth.js
@@ -1,6 +1,4 @@
-import {create} from 'zustand'
-import {mountStoreDevtool} from "simple-zustand-devtools";
-
+import { create } from "zustand";
 import { mountStoreDevtool } from "simple-zustand-devtools";
 
 const useAuthStore = create((set, get) => ({
