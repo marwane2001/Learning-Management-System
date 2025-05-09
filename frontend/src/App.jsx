9c7cb2506d946a0afd08939c474670450e07d107
marwanesk <marwanebenhadj@gmail.com>
2025-05-09 13:21:03 +0100

    added routes for the login / logout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import MainWrapper from "./layouts/MainWrapper";
 import Register from "./views/auth/Register";
+import Login from "./views/auth/Login";
+import Logout from "./views/auth/Logout";
 import { CartContext } from "./views/plugin/Context";
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
         <MainWrapper>
           <Routes>
             <Route path="/register/" element={<Register />} />
+              <Route path="/login/" element={<Login />} />
+              <Route path="/logout/" element={<Logout />} />
           </Routes>
         </MainWrapper>
       </BrowserRouter>
